d1a582d7a59b55ba4725847861018ad57caddc3d
Joselyn Alajo <jlp.alajo@yavirac.edu.ec>
2022-07-14 17:49:59 -0500

    first

---
+++ prueba.py
@@ -0,0 +1,27 @@
+import psycopg2 
+try:
+
+ conexion=psycopg2.connect(
+     host='localhost',
+        user='postgres',
+        password='2030',
+        database='sistemafactura'
+)
+ print('conexion exit ')
+ cursor=conexion.cursor()
+ cursor.execute("SELECT version()")
+ row=cursor.fetchone()
+ print(row)
+ cursor.execute("SELECT * FROM usuario")
+ row=cursor.fetchall()
+ for row in row:
+    print(row)
+except Exception as ex: 
+    print(ex)
+finally:
+    conexion.close()
+    print("conexion dos")
+
+
+
+
